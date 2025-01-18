9ffc0b2da32aa35576bc5acaf389de62aec23844
Doug Chimento <dchimento@gmail.com>
2025-01-18 20:33:07 +0200

    move solana wallet

--- cli/src/context.rs
+++
@@ -1,9 +0,0 @@
-use {console::Term, copypasta::ClipboardContext, monedero_solana::SolanaWallet};
-
-pub struct Context {
-    pub wallet: SolanaWallet,
-    pub term: Term,
-    pub clip: ClipboardContext,
-}
-
-impl Context {}
